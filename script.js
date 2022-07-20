55d2f76879d71ab8e74cb3c8006d9aface88827d
Avery Burne <burneav@gmail.com>
2022-07-19 20:48:09 -0400

    Added recangle block

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 var gameBoard = document.getElementById("game-canvas");
 var gameBoardContext = gameBoard.getContext('2d');
+gameBoardContext.fillStyle = 'lightblue';
+gameBoardContext.strokeStyle = 'darkblue';
+gameBoardContext.fillRect(100, 100, 20, 100);
+gameBoardContext.strokeRect(100, 100, 20, 100);
--- script.ts
+++ script.ts
@@ -1,2 +1,7 @@
 const gameBoard: any = document.getElementById("game-canvas")
 const gameBoardContext = gameBoard.getContext('2d')
+
+gameBoardContext.fillStyle = 'lightblue'
+gameBoardContext.strokeStyle = 'darkblue'
+gameBoardContext.fillRect(100, 100, 20, 100)
+gameBoardContext.strokeRect(100, 100, 20, 100);
