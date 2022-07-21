d36840bbc4211084c144ddd7bd697f83a0cb902f
Avery Burne <burneav@gmail.com>
2022-07-21 11:10:29 -0400

    Added start button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var gameBoard = document.getElementById("game-canvas");
 var gameBoardContext = gameBoard.getContext('2d');
-gameBoardContext.fillStyle = 'lightblue';
-gameBoardContext.strokeStyle = 'darkblue';
-gameBoardContext.fillRect(100, 100, 20, 100);
-gameBoardContext.strokeRect(100, 100, 20, 100);
+var draw = function () {
+    gameBoardContext.fillStyle = 'lightblue';
+    gameBoardContext.strokeStyle = 'darkblue';
+    gameBoardContext.fillRect(100, 100, 20, 100);
+    gameBoardContext.strokeRect(100, 100, 20, 100);
+};
--- script.ts
+++ script.ts
@@ -1,7 +1,10 @@
 const gameBoard: any = document.getElementById("game-canvas")
 const gameBoardContext = gameBoard.getContext('2d')
 
-gameBoardContext.fillStyle = 'lightblue'
-gameBoardContext.strokeStyle = 'darkblue'
-gameBoardContext.fillRect(100, 100, 20, 100)
-gameBoardContext.strokeRect(100, 100, 20, 100);
+const draw = () => {
+    console.log('drawing')
+    gameBoardContext.fillStyle = 'lightblue'
+    gameBoardContext.strokeStyle = 'darkblue'
+    gameBoardContext.fillRect(100, 100, 20, 100)
+    gameBoardContext.strokeRect(100, 100, 20, 100); 
+}
