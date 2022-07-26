c4f3e2da5560ed2a157fe287dc6770c1e1f010dc
Avery Burne <burneav@gmail.com>
2022-07-25 21:46:18 -0400

    Added second bar and ball

--- script.ts
+++ script.ts
@@ -9,4 +9,8 @@ const draw = () => {
     gameBoardContext.strokeStyle = 'darkblue'
     gameBoardContext.fillRect(100, 100, 20, 100)
     gameBoardContext.strokeRect(100, 100, 20, 100); 
+
+    gameBoardContext.fillRect(400, 100, 20, 100)
+    gameBoardContext.strokeRect(400, 100, 20, 100); 
+    gameBoardContext.arc(250, 100, 50, 0, 2 * Math.PI)
 }
