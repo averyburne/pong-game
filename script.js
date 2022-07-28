3cd9d2e87acec08bfd53e0e936ee186da3c0b38f
Avery Burne <burneav@gmail.com>
2022-07-28 17:20:22 -0400

    Added a clear canvas function
    Added it in changeDirection function so old shapes will disappear

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 exports.__esModule = true;
 var gameBoard = document.getElementById("game-canvas");
 var gameBoardContext = gameBoard.getContext('2d');
+var boardBackground = 'white';
+var boardBorder = 'black';
 var rightBlock = [400, 100, 20, 100];
 var leftBlock = [100, 100, 20, 100];
 var draw = function () {
@@ -14,6 +16,15 @@ var draw = function () {
     gameBoardContext.strokeRect.apply(gameBoardContext, rightBlock);
     gameBoardContext.arc(250, 100, 50, 0, 2 * Math.PI);
 };
+var clearCanvas = function () {
+    gameBoardContext.fillStyle = boardBackground;
+    //  Select the colour for the border of the canvas
+    gameBoardContext.strokestyle = boardBorder;
+    // Draw a "filled" rectangle to cover the entire canvas
+    gameBoardContext.fillRect(0, 0, gameBoard.width, gameBoard.height);
+    // Draw a "border" around the entire canvas
+    gameBoardContext.strokeRect(0, 0, gameBoard.width, gameBoard.height);
+};
 var changeDirection = function (e) {
     var UP_KEY = 38;
     var DOWN_KEY = 40;
@@ -23,6 +34,7 @@ var changeDirection = function (e) {
     if (keyPressed === UP_KEY) {
         rightBlock[1] -= 20;
     }
+    clearCanvas();
     draw();
 };
 document.addEventListener("keydown", changeDirection);
--- script.ts
+++ script.ts
@@ -2,6 +2,8 @@ export {}
 
 let gameBoard: any = document.getElementById("game-canvas")
 const gameBoardContext = gameBoard.getContext('2d')
+const boardBackground = 'white'
+const boardBorder = 'black'
 
 let rightBlock = [400, 100, 20, 100]
 let leftBlock = [100, 100, 20, 100]
@@ -18,6 +20,16 @@ const draw = () => {
     gameBoardContext.arc(250, 100, 50, 0, 2 * Math.PI)
 }
 
+const clearCanvas = () => {
+    gameBoardContext.fillStyle = boardBackground;
+    //  Select the colour for the border of the canvas
+    gameBoardContext.strokestyle = boardBorder;
+    // Draw a "filled" rectangle to cover the entire canvas
+    gameBoardContext.fillRect(0, 0, gameBoard.width, gameBoard.height);
+    // Draw a "border" around the entire canvas
+    gameBoardContext.strokeRect(0, 0, gameBoard.width, gameBoard.height);
+}
+
 const changeDirection = (e) => {
     const UP_KEY = 38;
     const DOWN_KEY = 40;
@@ -29,6 +41,7 @@ const changeDirection = (e) => {
     if (keyPressed === UP_KEY) {
         rightBlock[1] -= 20;
     }
+    clearCanvas();
     draw();
 }
 
